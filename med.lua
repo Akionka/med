0250685dc739adfc50c46196f689f5bf63687f3d
Akionka <42269025+Akionka@users.noreply.github.com>
2019-02-03 00:45:19 +0300

    Fix

--- med.lua
+++ med.lua
@@ -1,7 +1,7 @@
 script_name('Med')
 script_author('akionka')
-script_version('1.2')
-script_version_number(3)
+script_version('1.3')
+script_version_number(4)
 script_description([[Используйте аптечку через /med!]])
 
 local sampev = require 'lib.samp.events'
@@ -88,6 +88,7 @@ function goupdate()
 			sampAddChatMessage((u8:decode('[MED]: Новая версия установлена! Чтобы скрипт обновился нужно либо перезайти в игру, либо ...')), -1)
 			sampAddChatMessage((u8:decode('[MED]: ... если у вас есть автоперезагрузка скриптов, то новая версия уже готова и снизу вы увидите приветственное сообщение.')), -1)
 			sampAddChatMessage((u8:decode('[MED]: Если что-то пошло не так, то сообщите мне об этом в VK или Telegram > {2980b0}vk.com/akionka teleg.run/akionka{FFFFFF}.')), -1)
+			updateinprogess = false
 		end
 	end)
 end
