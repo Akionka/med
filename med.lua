3ec60e11aa0ac992818a1a5e381f5a851a8e760f
Akionka <42269025+Akionka@users.noreply.github.com>
2019-03-12 23:27:25 +0300

    New update system

--- med.lua
+++ med.lua
@@ -1,8 +1,7 @@
 script_name('Med')
 script_author('akionka')
-script_version('1.3')
-script_version_number(4)
-script_description([[Используйте аптечку через /med!]])
+script_version('1.4')
+script_version_number(5)
 
 local sampev = require 'lib.samp.events'
 local encoding = require 'encoding'
@@ -11,7 +10,8 @@ local dlstatus = require('moonloader').download_status
 encoding.default = 'cp1251'
 u8 = encoding.UTF8
 
-med = false
+local med = false
+local updatesavaliable = false
 
 local ini = inicfg.load({
 	settings = {
@@ -19,7 +19,7 @@ local ini = inicfg.load({
 	}
 }, "med")
 
-function sampev.onShowDialog(id, stytle, title, btn1, btn2, text)
+function sampev.onShowDialog(id, _, _, _, _, text)
 	if id == 1000 and med then
 		local i = 0
 		for item in text:gmatch("[^\r\n]+") do
@@ -45,13 +45,25 @@ function main()
 	if not isSampfuncsLoaded() or not isSampLoaded() then return end
 	while not isSampAvailable() do wait(0) end
 	sampAddChatMessage(u8:decode("[MED]: Скрипт {00FF00}успешно{FFFFFF} загружен. Версия: {2980b9}"..thisScript().version.."{FFFFFF}."), -1)
-	update()
-	while updateinprogess ~= false do wait(0) end
+
+	checkupdates('https://raw.githubusercontent.com/Akionka/med/master/version.json')
+
 	sampRegisterChatCommand("medinv", function()
 		ini.settings.invex = not ini.settings.invex
 		inicfg.save(ini, "med")
 		sampAddChatMessage(ini.settings.invex and u8:decode("[MED]: Скрипт теперь работает с {2980b9}/invex{FFFFFF}.") or u8:decode("[MED]: Скрипт теперь работает с {2980b9}/inv{FFFFFF}."), -1)
 	end)
+
+	sampRegisterChatCommand('medupdate', function()
+		if updatesavaliable then
+			update('https://raw.githubusercontent.com/Akionka/med/master/med.lua')
+		end
+	end)
+
+	sampRegisterChatCommand('medcheck', function()
+		checkupdates('https://raw.githubusercontent.com/Akionka/med/master/version.json')
+	end)
+
 	sampRegisterChatCommand("med", function()
 		if getCharHealth(PLAYER_PED) == 100 then return true end
 		med = true
@@ -59,36 +71,43 @@ function main()
 	end)
 end
 
-function update()
-	local fpath = os.getenv('TEMP') .. '\\MED-version.json'
-	downloadUrlToFile('https://raw.githubusercontent.com/Akionka/med/master/version.json', fpath, function(id, status, p1, p2)
-		if status == dlstatus.STATUS_ENDDOWNLOADDATA then
-			local f = io.open(fpath, 'r')
-			if f then
-				local info = decodeJson(f:read('*a'))
-				if info and info.version then
-					version = info.version
-					version_num = info.version_num
-					if version_num > thisScript().version_num then
-						sampAddChatMessage(u8:decode("[MED]: Найдено объявление. Текущая версия: {2980b9}"..thisScript().version.."{FFFFFF}, новая версия: {2980b9}"..version.."{FFFFFF}. Начинаю закачку."), -1)
-						lua_thread.create(goupdate)
+function checkupdates(json)
+	local fpath = os.getenv('TEMP')..'\\'..thisScript().name..'-version.json'
+	if doesFileExist(fpath) then os.remove(fpath) end
+	downloadUrlToFile(json, fpath, function(_, status, _, _)
+		if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+			if doesFileExist(fpath) then
+				local f = io.open(fpath, 'r')
+				if f then
+					local info = decodeJson(f:read('*a'))
+					local updateversion = info.version_num
+					f:close()
+					os.remove(fpath)
+					if updateversion > thisScript().version_num then
+						updatesavaliable = true
+						sampAddChatMessage(u8:decode("[MED]: Найдено объявление. Текущая версия: {2980b9}"..thisScript().version.."{FFFFFF}, новая версия: {2980b9}"..updateversion.."{FFFFFF}."), -1)
+						sampAddChatMessage(u8:decode("[MED]: Используйте команду {2980b0}/medupdate{FFFFFF}, чтобы обновиться до последней версии."), -1)
+						return true
 					else
-						updateinprogess = false
+						updatesavaliable = false
+						sampAddChatMessage(u8:decode("[MED]: У вас установлена самая свежая версия скрипта."), -1)
 					end
+				else
+					updatesavaliable = false
+					sampAddChatMessage(u8:decode("[MED]: Что-то пошло не так, упс. Попробуйте позже."), -1)
 				end
 			end
 		end
 	end)
 end
 
-function goupdate()
-	wait(300)
-	downloadUrlToFile("https://raw.githubusercontent.com/Akionka/med/master/med.lua", thisScript().path, function(id3, status1, p13, p23)
+function update(url)
+	downloadUrlToFile(url, thisScript().path, function(_, status1, _, _)
 		if status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
-			sampAddChatMessage((u8:decode('[MED]: Новая версия установлена! Чтобы скрипт обновился нужно либо перезайти в игру, либо ...')), -1)
-			sampAddChatMessage((u8:decode('[MED]: ... если у вас есть автоперезагрузка скриптов, то новая версия уже готова и снизу вы увидите приветственное сообщение.')), -1)
-			sampAddChatMessage((u8:decode('[MED]: Если что-то пошло не так, то сообщите мне об этом в VK или Telegram > {2980b0}vk.com/akionka teleg.run/akionka{FFFFFF}.')), -1)
-			updateinprogess = false
+			sampAddChatMessage(u8:decode('[MED]: Новая версия установлена! Чтобы скрипт обновился нужно либо перезайти в игру, либо ...'), -1)
+			sampAddChatMessage(u8:decode('[MED]: ... если у вас есть автоперезагрузка скриптов, то новая версия уже готова и снизу вы увидите приветственное сообщение.'), -1)
+			sampAddChatMessage(u8:decode('[MED]: Если что-то пошло не так, то сообщите мне об этом в VK или Telegram > {2980b0}vk.com/akionka teleg.run/akionka{FFFFFF}.'), -1)
+			thisScript():reload()
 		end
 	end)
 end
